b928241687acc08dd5a137e3c65aabcc51b4d30a
Parker Boutell <pboutell@gmail.com>
2013-08-11 17:43:24 -0600

    add files

---
+++ amud-server/Character.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace amud_server
+{
+    class Character
+    {
+        public CharacterStats stats;
+
+        public void takeDamage(int damage)
+        {
+            stats.health -= damage;
+        }
+    }
+}
---
+++ amud-server/NPC.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace amud_server
+{
+    class NPC : Character
+    {
+        public string name { get; private set; }
+        public string decription { get; private set; }
+
+        public NPC(string name, string description, CharacterStats stats)
+        {
+            this.name = name;
+            this.decription = decription;
+            this.stats = stats;
+        }
+    }
+}
