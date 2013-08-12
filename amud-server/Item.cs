a841cbb7cf288ec2f3f1dbca848ab107ec4d2942
Parker Boutell <pboutell@gmail.com>
2013-08-11 21:36:40 -0600

    add basic Item code

---
+++ amud-server/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace amud_server
+{
+    class Item
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public int weight;
+        public string wearLocation;
+
+        public Item(string name, string description, int weight, string wearLocation)
+        {
+            this.name = name;
+            this.description = description;
+            this.weight = weight;
+            this.wearLocation = wearLocation;
+        }
+    }
+}
