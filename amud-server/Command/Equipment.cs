a66eca0b579de7080249b2615a57f4ab2faf70a4
Parker Boutell <pboutell@gmail.com>
2013-08-11 22:53:00 -0600

    remove, drop, wear, equip, and inventory for items

---
+++ amud-server/Command/Equipment.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace amud_server
+{
+    partial class Commands
+    {
+        private void doWear(string[] args, Player player)
+        {
+            if (args.Length == 1)
+            {
+                player.client.send("\r\nWear what?");
+            }
+            else
+            {
+                Item item = player.items.getItemByName(args[1].TrimEnd('\r', '\n'));
+
+                if (item != null)
+                {
+                    if (player.items.addToEquipped(item.wearLocation, item))
+                    {
+                        player.client.send("\r\nYou equip " + item.description + ".\r\n");
+                    }
+                    else
+                    {
+                        player.client.send("\r\nYou can't wear that there!\r\n");
+                    }
+                }
+            }
+        }
+
+        private void doInventory(string[] args, Player player)
+        {
+            player.client.send(player.items.inventoryToString());
+        }
+
+        private void doEquipment(string[] args, Player player)
+        {
+            player.client.send(player.items.equippedToString());
+        }
+
+        private void doDrop(string[] args, Player player)
+        {
+            StringBuilder buffer = new StringBuilder();
+
+            if (args.Length == 1)
+            {
+                player.client.send("\r\nDrop what?");
+            }
+            else
+            {
+                Item item = player.items.getItemByName(args[1].TrimEnd('\r', '\n'));
+
+                if (item != null)
+                {
+                    player.items.removeFromInventory(item);
+                    player.room.items.Add(item);
+                    buffer.AppendFormat("You drop a {0} onto the ground\r\n", item.description);
+                    player.client.send(buffer.ToString());
+                }
+                else
+                {
+                    player.client.send("\r\nI can't find that item");
+                }
+            }
+        }
+
+        private void doRemove(string[] args, Player player)
+        {
+            StringBuilder buffer = new StringBuilder();
+
+            if (args.Length == 1)
+            {
+                player.client.send("\r\nRemove what?");
+            }
+            else
+            {
+                Item item = player.items.removeFromEquipped(args[1].TrimEnd('\r', '\n'));
+
+                if (item != null)
+                {
+                    buffer.AppendFormat("\r\nRemoved {0} from {1}.\r\n", item.description, item.wearLocation);
+                    player.client.send(buffer.ToString());
+                }
+                else
+                {
+                    player.client.send("\r\nI don't know how to remove that");
+                }
+            }
+
+        }
+    }
+}
---
+++ amud-server/Inventory.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace amud_server
+{
+    class Inventory
+    {
+        private Dictionary<string, Item> equipped = new Dictionary<string, Item>();
+        private List<Item> inventory = new List<Item>();
+
+        public void addToInventory(Item item)
+        {
+            inventory.Add(item);
+        }
+
+        public void removeFromInventory(Item item)
+        {
+            inventory.Remove(item);
+        }
+
+        public Item removeFromEquipped(string search)
+        {
+            Item item = null;
+
+            foreach(KeyValuePair<string, Item> e in equipped)  
+            {
+                if (e.Value.name == search)
+                {
+                    equipped.TryGetValue(e.Key, out item);
+                    item = e.Value;
+                }
+            }
+            if (item != null)
+            {
+                equipped.Remove(item.wearLocation);
+                inventory.Add(item);
+            }
+
+            return item;
+        }
+
+        public bool addToEquipped(string location, Item item)
+        {
+            if (!equipped.Keys.Contains(location))
+            {
+                equipped.Add(location, item);
+                inventory.Remove(item);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public string inventoryToString()
+        {
+            StringBuilder buffer = new StringBuilder();
+
+            buffer.AppendLine("\r\nInventory:");
+            foreach (Item i in inventory)
+            {
+                buffer.AppendFormat("  {0}\t{1}\r\n", i.name, i.description);
+            }
+
+            return buffer.ToString();
+        }
+
+        public string equippedToString()
+        {
+            StringBuilder buffer = new StringBuilder();
+
+            buffer.AppendLine("\r\nEquipment:");
+            foreach (KeyValuePair<string, Item> e in equipped)
+            {
+                buffer.AppendFormat("  [ {0} ]\t{1}\r\n", e.Key, e.Value.description);
+            }
+
+            return buffer.ToString();
+        }
+
+        public Item getItemByName(string search)
+        {
+            foreach (Item i in inventory)
+            {
+                if (i.name == search)
+                {
+                    return i;
+                }
+            }
+
+            return null;
+        }
+
+    }
+}
