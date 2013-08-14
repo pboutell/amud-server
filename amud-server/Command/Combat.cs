f9b7901b56994491e3c66e18e4517444d1cbc83b
Parker Boutell <pboutell@gmail.com>
2013-08-13 23:06:07 -0600

    added kill command that does nothing

---
+++ amud-server/Command/Combat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace amud_server
+{
+    partial class Commands
+    {
+        private void doKill(string[] args, Player player) 
+        {
+            if (args.Length == 1)
+            {
+                player.client.send("Kill whom?");
+                return;
+            }
+
+            NPC target = player.room.getNPCByName(args[1]);
+
+            if (target != null)
+            {
+            }
+            else
+            {
+                player.client.send("I do not see anybody by that name here.");
+            }
+        }
+    }
+}
