35eaab86ac36b14080721b83f3e8b26034a0b0dd
Parker Boutell <pboutell@gmail.com>
2013-08-17 00:52:55 -0600

    rename CharacterStats to Stats

---
+++ amud-server/Character/Stats.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace amud_server
+{
+    [Serializable]
+    public class Stats
+    {
+        public int maxHealth { get; private set; }
+        public int health { get; set; }
+
+        public int maxMana { get; private set; }
+        public int mana { get; set; }
+
+        public Stats(int maxHealth, int maxMana)
+        {
+            this.maxHealth = maxHealth;
+            this.maxMana = maxMana;
+
+            this.health = maxHealth;
+            this.mana = maxMana;
+        }
+    }
+}
